56af26463ed640ce8dbe213284d32b7c3fb36527
nivpc <niv.goldenberg@gmail.com>
2017-04-27 00:12:29 +0300

    Add support for new spinner design.

--- AngularBase/app/app.module.ts
+++ AngularBase/app/app.module.ts
@@ -3,17 +3,22 @@
  */
 import IAngularStatic = angular.IAngularStatic;
 import {appComponent} from "./app.component";
-import {AccountModule} from "./account/account.module";
+import {accountModule} from "./account/account.module";
 import {SFUtilsSelectorService} from "./sfutils/sfUtilsSelector.service";
 import {SFUtilsServer} from "./sfutils/sfUtilsServer.server";
 import {SFUtilsMock} from "./sfutils/sfUtilsMock.service";
+import {recordsTableModule} from "./recordsTable/recordsTable.module";
 declare const angular: IAngularStatic;
 
 export const appModule = angular
-    .module('app.module', [
+    .module('appModule', [
         'cgBusy', //https://github.com/cgross/angular-busy/blob/master/README.md
-        AccountModule.name
+        accountModule.name,
+        recordsTableModule.name
     ])
+    .value('cgBusyDefaults',{
+        templateUrl: SFUtilsSelectorService.buildTemplateURL('app/spinner/customSpinner.html')
+    })
     .component('myApp', appComponent)
     .service('sfUtilsServiceSelector', SFUtilsSelectorService)
     .service('sfServer', SFUtilsServer)
